426fd49549b9c449c426fc69b241fd32ecdd1da6
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2020-11-23 02:08:08 +0100

    Updates

---
+++ precache-manifest.1ede3beda1ec39ac38ed7e0495d80c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "704e6b16fd95dfee70f1a9794cf81c60",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "68ba9b56c437893c5395",
+    "url": "/weather-app/static/css/main.84d4f2de.chunk.css"
+  },
+  {
+    "revision": "367b27b49e9f9b3216eb",
+    "url": "/weather-app/static/js/2.bc7f8f2a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/weather-app/static/js/2.bc7f8f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ba9b56c437893c5395",
+    "url": "/weather-app/static/js/main.9d059144.chunk.js"
+  },
+  {
+    "revision": "d87847414576153e11b6",
+    "url": "/weather-app/static/js/runtime-main.4e2bb22d.js"
+  }
+]);
